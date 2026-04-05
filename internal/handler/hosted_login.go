8c18b2a1e0b06b1ecc683e49cb1278d3150ff903
alghanim <alialghanim89@gmail.com>
2026-04-05 10:47:04 +0300

    Fix CSRF cookie Secure flag when TLS is disabled
    The CSRF cookie was hardcoded with Secure: true, which prevents
    the browser from sending it back over HTTP. When AUTH_TLS_DISABLED
    is true, the cookie should not be marked Secure, otherwise the
    hosted login page always fails with 'invalid CSRF token'.

--- internal/handler/hosted_login.go
+++ internal/handler/hosted_login.go
@@ -16,14 +16,14 @@ func generateCSRFToken() string {
 	return hex.EncodeToString(b)
 }
 
-func setCSRFCookie(w http.ResponseWriter, token string) {
+func setCSRFCookie(w http.ResponseWriter, token string, secure bool) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "__csrf",
 		Value:    token,
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteStrictMode,
-		Secure:   true,
+		Secure:   secure,
 	})
 }
 
@@ -70,7 +70,7 @@ func (h *Handler) handleHostedLoginPage(w http.ResponseWriter, r *http.Request) 
 	}
 
 	csrfToken := generateCSRFToken()
-	setCSRFCookie(w, csrfToken)
+	setCSRFCookie(w, csrfToken, !h.cfg.TLSDisabled)
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	// %[1]s = redirectURI, %[2]s = errorHTML, %[3]s = ssoLink, %[4]s = csrfToken,
